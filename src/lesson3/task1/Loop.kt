039628187fe17cedc4f88d3e051506d91c336790
hvorostovsa <inga.egorova@waveaccess.ru>
2021-09-21 14:12:04 +0300

    t

--- src/lesson3/task1/Loop.kt
+++ src/lesson3/task1/Loop.kt
@@ -79,7 +79,7 @@ fun digitNumber(n: Int): Int {
     do {
         k++
         number /= 10
-    } while (number > 0)
+    } while (number != 0)
     return k
 }
 
@@ -89,12 +89,13 @@ fun digitNumber(n: Int): Int {
  * Найти число Фибоначчи из ряда 1, 1, 2, 3, 5, 8, 13, 21, ... с номером n.
  * Ряд Фибоначчи определён следующим образом: fib(1) = 1, fib(2) = 1, fib(n+2) = fib(n) + fib(n+1)
  */
-fun fib(n: Int): Int =
-    when (n) {
-        in 1..2 -> 1
-        else -> fib(n - 2) + fib(n - 1)
+fun fib(n: Int): Int {
+    var num = 1
+    for (i in 1 until n) {
+        num += num
     }
-
+    return num
+}
 /**
  * Простая (2 балла)
  *
