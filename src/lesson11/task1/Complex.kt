5c1707dd18ef0baa2dc2907b22ba647a8bdd1d0a
Shilonosov Alexandr <schilonosov.alex@yandex.ru>
2021-11-21 18:43:27 +0300

    Doing lesson

--- src/lesson11/task1/Complex.kt
+++ src/lesson11/task1/Complex.kt
@@ -2,10 +2,25 @@
 
 package lesson11.task1
 
+import kotlin.math.pow
+
 /**
  * Фабричный метод для создания комплексного числа из строки вида x+yi
  */
-fun Complex(s: String): Complex = TODO()
+fun Complex(s: String): Complex {
+    try {
+        if (!s.endsWith("i")) throw IllegalArgumentException()
+        val signIndex = s.indexOfFirst { it == '+' || it == '-' }
+        if (signIndex == -1) throw IllegalArgumentException()
+        return Complex(
+            s.substring(0, signIndex).toDouble(),
+            s.substring(signIndex, s.length - 1).toDouble()
+        )
+    } catch (e: Exception) {
+        throw IllegalArgumentException("Incorrect complex number")
+    }
+
+}
 
 /**
  * Класс "комплексное число".
@@ -21,40 +36,49 @@ class Complex(val re: Double, val im: Double) {
     /**
      * Конструктор из вещественного числа
      */
-    constructor(x: Double) : this(TODO(), TODO())
+    constructor(x: Double) : this(x, 0.0)
 
     /**
      * Сложение.
      */
-    operator fun plus(other: Complex): Complex = TODO()
+    operator fun plus(other: Complex): Complex = Complex(re + other.re, im + other.im)
 
     /**
      * Смена знака (у обеих частей числа)
      */
-    operator fun unaryMinus(): Complex = TODO()
+    operator fun unaryMinus(): Complex = Complex(-re, -im)
 
     /**
      * Вычитание
      */
-    operator fun minus(other: Complex): Complex = TODO()
+    operator fun minus(other: Complex): Complex = this + -other
 
     /**
      * Умножение
      */
-    operator fun times(other: Complex): Complex = TODO()
+    operator fun times(other: Complex): Complex = Complex(
+        re * other.re - im * other.im,
+        re * other.im + im * other.re
+    )
 
     /**
      * Деление
      */
-    operator fun div(other: Complex): Complex = TODO()
+    operator fun div(other: Complex): Complex {
+        val divider = other.re.pow(2) + other.im.pow(2)
+        return Complex(
+            (re * other.re + im * other.im) / divider,
+            (im * other.re - re * other.im) / divider
+        )
+    }
 
     /**
      * Сравнение на равенство
      */
-    override fun equals(other: Any?): Boolean = TODO()
+    override fun equals(other: Any?): Boolean = other is Complex && (re == other.re && im == other.im)
 
     /**
      * Преобразование в строку
      */
-    override fun toString(): String = TODO()
+    override fun toString(): String = "$re+${im}i"
 }
